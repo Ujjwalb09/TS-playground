798ae1867a04ba80a7c744b9b5d20ccac6232f1f
ujjwalb09 <ujjwal.js.dev@gmail.com>
2025-04-15 17:03:07 +0530

    started with TS

---
+++ app.ts
@@ -0,0 +1,10 @@
+// typescript have type safety
+
+// npm i typescript -g,   to install typecript compiler globally
+
+//tsc --init, to create a config file
+
+//tsc --watch, will compile the ts code in real time
+
+var a = 12;
+var a  = 10;
